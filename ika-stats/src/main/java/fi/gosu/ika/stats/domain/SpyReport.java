be88951e8bc1bad4dd4dab1958d996d49686f560
Ville Nupponen <aikain@gosu.fi>
2017-06-06 16:24:01 +0300

    Remove foreignKey check from SpyReport's town.

--- ika-stats/src/main/java/fi/gosu/ika/stats/domain/SpyReport.java
+++ ika-stats/src/main/java/fi/gosu/ika/stats/domain/SpyReport.java
@@ -19,7 +19,7 @@ public class SpyReport extends AbstractPersistable<Long> {
     private Long spyReportId;
     private Long townId;
     @OneToOne
-    @JoinColumn(name="townId", insertable = false, updatable = false, foreignKey = @ForeignKey(name = "none"))
+    @JoinColumn(name="townId", insertable = false, updatable = false, foreignKey = @ForeignKey(name = "none", value = ConstraintMode.NO_CONSTRAINT))
     private Town town;
     private String playerName;
     private String alliance;
