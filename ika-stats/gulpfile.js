b254cc0293498a5e949fd2c72f950b65dc582548
Ville Nupponen <aikain@gosu.fi>
2017-06-06 14:08:09 +0300

    Fix typos

--- ika-stats/gulpfile.js
+++ ika-stats/gulpfile.js
@@ -29,13 +29,14 @@ gulp.task('js', function() {
 });
 
 gulp.task("css", function () {
-    return gulp.src(WEB_ROOT + "assets/sass/**")
+    return gulp.src(WEB_ROOT + "assets/sass/**.scss")
         .pipe(sass().on("error", sass.logError))
         .pipe(gulp.dest(WEB_ROOT + "public/css"));
 });
 
 gulp.task('watch', function() {
-    gulp.watch(WEB_ROOT + 'assets/js/src/**/*.{js,jsx}', ['concat']);
+    gulp.watch(WEB_ROOT + 'assets/js/src/**.{js,jsx}', ['js']);
+    gulp.watch(WEB_ROOT + 'assets/sass/**.scss', ['css']);
 });
 
 gulp.task('build', ['libs', 'js', 'css']);
--- ika-stats/src/main/webapp/assets/js/src/index.jsx
+++ ika-stats/src/main/webapp/assets/js/src/index.jsx
@@ -2,6 +2,6 @@
  * Created by Aikain on 6.6.2017.
  */
 ReactDOM.render(
-<h1>Hello, world2!</h1>,
+<h1>Hello, world!</h1>,
     document.getElementById('app')
 );
