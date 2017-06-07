d5cac9c95bc653cf4c67ea65ece6f6997f371e4f
Ville Nupponen <aikain@gosu.fi>
2017-06-07 12:47:02 +0300

    Concat multiple sass/css files. Minify/clean css & js.

--- ika-stats/gulpfile.js
+++ ika-stats/gulpfile.js
@@ -1,7 +1,12 @@
-var gulp        = require('gulp');
-var sass        = require('gulp-sass');
-var browserify  = require('browserify');
-var source      = require('vinyl-source-stream');
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var browserify   = require('browserify');
+var source       = require('vinyl-source-stream');
+var uglify       = require('gulp-uglify');
+var cleanCSS     = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
+var concatCSS    = require('gulp-concat-css');
+var streamify    = require('gulp-streamify');
 
 var WEB_ROOT = 'src/main/webapp/';
 
@@ -9,13 +14,17 @@ gulp.task('js', function() {
     browserify({ entries: WEB_ROOT + 'assets/js/src/App.js' })
         .transform("babelify", {presets: ["es2015", "react"]})
         .bundle()
-        .pipe(source('bundle.js'))
+        .pipe(source('bundle.min.js'))
+        .pipe(streamify(uglify()))
         .pipe(gulp.dest(WEB_ROOT + 'public/js/'));
 });
 
 gulp.task("css", function () {
     return gulp.src(WEB_ROOT + "assets/sass/**.scss")
         .pipe(sass().on("error", sass.logError))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+        .pipe(concatCSS('bundle.min.css'))
+        .pipe(cleanCSS())
         .pipe(gulp.dest(WEB_ROOT + "public/css"));
 });
 
